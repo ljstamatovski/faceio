be317ebb1586dff7497ffa655e6f90579ac5b373
Љубиша Стаматовски <ljubisastamatovski@hotmail.com>
2022-07-18 20:52:47 +0200

    Added endpoint for creating group

---
+++ src/Web/FaceIO.Commands/Group/AddGroupCommand.cs
@@ -0,0 +1,54 @@
+﻿namespace FaceIO.Commands.Group
+{
+    using Contracts.Common.Database.Context;
+    using Domain.Customer.Repositories;
+    using Domain.Customer.Entities;
+    using Domain.Group.Entities;
+    using MediatR;
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class AddGroupCommand : IRequest
+    {
+        public Guid CustomerUid { get; }
+
+        public string Name { get; }
+
+        public string? Description { get; }
+
+        public AddGroupCommand(Guid customerUid, string name, string? description)
+        {
+            CustomerUid = customerUid;
+            Name = name;
+            Description = description;
+        }
+    }
+
+    public class AddGroupCommandHandler : IRequestHandler<AddGroupCommand>
+    {
+        private readonly IFaceIODbContext _dbContext;
+        private readonly ICustomersRepository _customersRepository;
+
+        public AddGroupCommandHandler(IFaceIODbContext dbContext, ICustomersRepository customersRepository)
+        {
+            _dbContext = dbContext;
+            _customersRepository = customersRepository;
+        }
+
+        public async Task<Unit> Handle(AddGroupCommand request, CancellationToken cancellationToken)
+        {
+            Customer customer = await _customersRepository.GetCustomerAsync(request.CustomerUid);
+
+            Group group = Group.Factory.Create(name: request.Name,
+                                               request.Description,
+                                               customer.Id);
+
+            customer.Groups.Add(group);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Web/FaceIO.Contracts/Group/CreateGroupRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FaceIO.Contracts.Group
+{
+    public class CreateGroupRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+    }
+}
