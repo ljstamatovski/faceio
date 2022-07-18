ee7c5acb0600ca059271e98391c5b296fd34b86b
Љубиша Стаматовски <ljubisastamatovski@hotmail.com>
2022-07-18 21:02:47 +0200

    Added get groups endpoint

---
+++ src/Web/FaceIO.Contracts/Group/GroupDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FaceIO.Contracts.Group
+{
+    using Common;
+
+    public class GroupDto : RestBase
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+    }
+}
