88480cb37da7397518bb82ead1b8f8d6deec47b6
Љубиша Стаматовски <ljubisastamatovski@hotmail.com>
2022-07-22 23:47:21 +0200

    Added get group locations endpoint

---
+++ src/Web/FaceIO.Contracts/GroupLocation/GroupLocationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FaceIO.Contracts.GroupLocation
+{
+    using Common;
+    using System;
+
+    public class GroupLocationDto : RestBase
+    {
+        public Guid GroupUid { get; set; }
+
+        public Guid LocationUid { get; set; }
+    }
+}
