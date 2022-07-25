e8b9a1a336e38e36f5866b00a4f71a9081fc79ec
Љубиша Стаматовски <ljubisastamatovski@hotmail.com>
2022-07-25 20:28:54 +0200

    Added endpoint for uploading image to s3

---
+++ src/Web/FaceIO.Api/Controllers/PersonImagesController.cs
@@ -0,0 +1,34 @@
+﻿namespace FaceIO.Api.Controllers
+{
+    using FaceIO.Commands.PersonImage;
+    using FaceIO.Contracts.Common;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/customers/{customerUid:guid}/persons/{personUid:guid}")]
+    public class PersonImagesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PersonImagesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("image")]
+        public async Task<IActionResult> UploadPersonImageAsync([FromRoute] Guid customerUid, [FromRoute] Guid personUid, IFormFile image)
+        {
+            var imageRequest = new UploadImageRequest
+            {
+                Name = image.Name,
+                FileName = image.FileName,
+                ContentType = image.ContentType,
+                FileStream = image.OpenReadStream()
+            };
+
+            return Ok(await _mediator.Send(new AddPersonImageCommand(customerUid: customerUid, personUid: personUid, imageRequest: imageRequest)));
+        }
+    }
+}
---
+++ src/Web/FaceIO.Contracts/Common/Settings/IAwsSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace FaceIO.Contracts.Common.Settings
+{
+    public interface IAwsSettings
+    {
+        string BucketName { get; set; }
+
+        string AccessKey { get; set; }
+
+        string AccessSecretKey { get; set; }
+    }
+}
